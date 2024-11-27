22853eb8b2fbc64963d734fba645ebe4ff5b7022
akullladdha <akul.laddha@gmail.com>
2024-11-27 18:57:30 -0500

    added lambda expression

--- AkulAnishQuiz/src/Main.java
+++ AkulAnishQuiz/src/Main.java
@@ -2,7 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        I addition = (a,b)->{
+            int count=0;
+            for(int i=a; i<=b; i++) {
+                count+=i;
+            }
+            return count;
+        };
+
+        try {
+            int a  = Integer.parseInt(args[0]);
+            int b = Integer.parseInt(args[1]);
+            System.out.println(addition.add(a,b));
+        } catch (NumberFormatException e) {
+            System.out.println("error, enter integers");
+        }
+
     }
 }
